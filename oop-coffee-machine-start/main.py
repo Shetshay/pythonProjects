5906b728ed006033b303f045766a863316a53036
Andrew Attia Ibrahim <andrewattiaibrahim@stg-edu-136-168-49-138.dyn.csub.edu>
2024-12-17 15:01:49 -0800

    finish oop coffeemachine program

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -6,8 +6,6 @@ machine = MoneyMachine()
 menu = Menu()
 coffee = CoffeeMaker()
 
-#choice = MenuItem()
-#print(menu.menu[0].name)
 items = menu.get_items() #displays all items
 
 def findItem(nameOfItem):
@@ -15,6 +13,7 @@ def findItem(nameOfItem):
     # menu class
     for menuItem in menu.menu:
         if menuItem.name == nameOfItem:
+            #print(menuItem.name)
             return menuItem
     return False
 
@@ -22,20 +21,19 @@ def findCost(nameOfItem): # find the cost of the menu.menu item
     for cost in menu.menu:
         if cost.name == nameOfItem:
             return cost.cost # unlike the function above, we want to return the
-        # cost when we find the name, since its in a dictionary, we already have its name already since
+        # cost when we find the name, since it's in a dictionary, we already have its name already since
         # its name is equal to our choice, then we add .cost at the end to get the cost of the item
     return False
 
-
-
-while order != "off":
+while order != "off": # START PROGRAM
     choice = str(input(f"What would you like? ({items})"))
     if menu.find_drink(choice):
         choiceOfUser = findItem(choice)
         if coffee.is_resource_sufficient(choiceOfUser):
-            total = machine.process_coins() # gets the calculation of cost
-            machine.make_payment(findCost(choice)) # gets your change and does the math
+            #total = machine.process_coins() # gets the calculation of cost
+            if machine.make_payment(findCost(choice)): # gets your change and does the math
+                coffee.make_coffee(choiceOfUser)
 
-    if choice == 'r':
+    if choice == 'r': # SHOW UPDATES
         machine.report()
         coffee.report()
