dfd06d0f4f9f9864a2336ec138ea92f034ec93e6
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2024-12-18 12:12:37 -0800

    use less lines, add comments for clarification

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -5,35 +5,20 @@ order = ''
 machine = MoneyMachine()
 menu = Menu()
 coffee = CoffeeMaker()
-
-items = menu.get_items() #displays all items
-
-def findItem(nameOfItem):
-    # menu.menu is an array of MenuItems in the
-    # menu class
-    for menuItem in menu.menu:
-        if menuItem.name == nameOfItem:
-            #print(menuItem.name)
-            return menuItem
-    return False
-
-def findCost(nameOfItem): # find the cost of the menu.menu item
-    for cost in menu.menu:
-        if cost.name == nameOfItem:
-            return cost.cost # unlike the function above, we want to return the
-        # cost when we find the name, since it's in a dictionary, we already have its name already since
-        # its name is equal to our choice, then we add .cost at the end to get the cost of the item
-    return False
-
+drink = 0.0 # SET TO FLOAT
+items = menu.get_items() # VARIABLE TO DISPLAY ALL ITEMS
 while order != "off": # START PROGRAM
     choice = str(input(f"What would you like? ({items})"))
-    if menu.find_drink(choice):
-        choiceOfUser = findItem(choice)
-        if coffee.is_resource_sufficient(choiceOfUser):
-            #total = machine.process_coins() # gets the calculation of cost
-            if machine.make_payment(findCost(choice)): # gets your change and does the math
-                coffee.make_coffee(choiceOfUser)
-
+    drinkChosen = menu.find_drink(choice) #THE DRINK WE CHOSE, IS IN THIS OBJECT VARIABLE
+    if menu.find_drink(choice): # IF THE DRINK THEY CHOSE IS IN THE MENU
+        if coffee.is_resource_sufficient(menu.find_drink(choice)): # IF THERE IS ENOUGH RESOURCES
+            # TO MAKE THIS OBJECT  & IT FOUND THE DRINK IN THE MENU
+            for item in menu.menu: # GO THROUGH THE MENU AND IF DRINK CHOSEN = AN ITEM ON THE MENU
+                if item == drinkChosen:
+                    drink = item.cost # SET DRINK TO THE COST OF THE DRINK
+                    #print(drink)
+            if machine.make_payment(drink): # gets your change and does the math
+                coffee.make_coffee(drinkChosen)
     if choice == 'r': # SHOW UPDATES
         machine.report()
         coffee.report()
