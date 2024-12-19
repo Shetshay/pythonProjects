86de0adb75c666879ae9ae8eba1ad35a62891918
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2024-12-19 11:44:19 -0800

    add scoring and answer checking

--- day-17-start/quiz-game-start/quiz_brain.py
+++ day-17-start/quiz-game-start/quiz_brain.py
@@ -3,16 +3,27 @@ class QuizBrain:
     def __init__(self, question_list):
         self.q_list = question_list
         self.question_number = 0
+        self.score = 0
 
     def NextQuestion(self):
         current_question = self.q_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+        self.check_answer(user_answer, current_question.answer)
 
     def still_has_questions(self):
         #print(len(self.q_list))
         #print(self.question_number)
         return len(self.q_list) > self.question_number
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was {correct_answer}.")
+        print(f"This is your current score: {self.score}")
 
 #QuizBrain(question_bank).NextQuestion()
 
