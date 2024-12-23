fecd04fbe256d1185d45e41834a3a1428eca9155
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2024-12-22 22:01:57 -0800

    generate random colors for each shape

--- turtleGUI/main.py
+++ turtleGUI/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 tim.shape("arrow")
@@ -16,9 +17,14 @@ tim.shape("arrow")
 counter = 3 # start at triangle
 while counter < 10: # end at decagon
     for _ in range (counter): # draw each shape, once done with current shape move to the next
-        tim.forward(40) # draw forward
+        tim.forward(100) # draw forward
         tim.right(360/counter) # calculate angle to start rotating to correctly draw the shape
     counter += 1 # add 1 side to the shape we are drawing until we reach 10 (decagon)
+    r = random.uniform(0.1, 1.0)
+    g = random.uniform(0.1, 1.0)
+    b = random.uniform(0.1, 1.0) # generate random colors
+    colorChange = (r, g, b)
+    tim.pencolor(colorChange) # change color
 
 screen = Screen()
-screen.exitonclick()
+screen.exitonclick() #close app on click
