c4da1d493440e3b03a4e9d06ef5341c64f4f270b
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2024-12-25 15:48:50 -0800

    finish spirograph code

--- turtleGUI/spirograph.py
+++ turtleGUI/spirograph.py
@@ -4,9 +4,28 @@ import random
 
 tim = Turtle()
 turtle.colormode(255)
+setAngle = 0
 
-tim.speed(10)
-turtle.circle(50)
+
+def randomColor():
+    r = random.randint(1, 255)
+    g = random.randint(1, 255)
+    b = random.randint(1, 255)
+    rgb = r, g, b
+    return rgb
+
+tim.speed(0)
+
+turtle.color(randomColor())
+
+while setAngle > -364:
+    tim.color(randomColor())
+    tim.circle(80)
+    tim.seth(setAngle)
+    setAngle -= 4
+
+
+#tim.circle(50)
 
 
 
