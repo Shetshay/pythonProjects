a3dc3a06da1cd5135803993e4c500942da2b198e
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2024-12-24 18:26:02 -0800

    starter code for creating a spirograph

---
+++ turtleGUI/spirograph.py
@@ -0,0 +1,14 @@
+import turtle
+from turtle import Turtle, Screen
+import random
+
+tim = Turtle()
+turtle.colormode(255)
+
+tim.speed(10)
+turtle.circle(50)
+
+
+
+screen = Screen()
+screen.exitonclick() #close app on click
