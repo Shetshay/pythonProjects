69a1dc9a6073225a4b279cf7d1d5cdccfabd8444
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2024-12-31 18:03:45 -0800

    create turtle standby for race

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
+
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
-print(user_bet)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+def create_turtle():
+    distance = 100
+    color_selection = 0
+    for color in colors:
+        color = Turtle(shape="turtle")
+        color.penup()
+        color.color((colors[color_selection]))
+        color.goto(x=-230, y=-distance)
+        distance -= 40
+        color_selection += 1
+
+
+# tim = Turtle(shape="turtle")
+# tim.color(random.choice(colors))
+# tim.penup()
+# tim.goto(x=-230,y=-100)
+
+create_turtle()
+
 screen.exitonclick()
 
