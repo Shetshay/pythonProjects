791a87b84c2961afe52fc5c4c65d2e3f022a4312
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2025-01-04 12:03:45 -0800

    create snake collision with food

--- snake-game/snake_game.py
+++ snake-game/snake_game.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "w")
@@ -22,5 +24,8 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
