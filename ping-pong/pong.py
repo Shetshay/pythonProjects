27d257f520d594929418e58f7cefeb7fc3d9d580
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2025-01-06 14:16:39 -0800

    move paddle on right and set it up

--- ping-pong/pong.py
+++ ping-pong/pong.py
@@ -4,21 +4,42 @@ import random
 import time
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Ping Pong")
-screen.tracer(0)
+#screen.tracer(0)
 
 game_is_on = True
 
-rightPlayer = RightPlayer()
+#rightPlayer = RightPlayer()
 #leftPlayer = left_player()
 
+pong = Turtle()
+
+width = 20
+height = 100
+
+pong.speed("fastest")
+pong.penup()
+pong.shape("square")
+pong.color("white")
+pong.shapesize(stretch_wid=5,stretch_len=1)
+pong.goto(350, 0)
+
+
 screen.listen()
+
+def up():
+    new_y = pong.ycor() + 20
+    pong.goto(pong.xcor(), new_y)
+
+def down():
+    new_y = pong.ycor() - 20
+    pong.goto(pong.xcor(), new_y)
 # screen.onkey(left_player.up, "w")
 # screen.onkey(left_player.down, "s")
-screen.onkey(rightPlayer.up, "i")
-screen.onkey(rightPlayer.down, "k")
+screen.onkey(up, "i")
+screen.onkey(down, "k")
 
 
 while game_is_on:
