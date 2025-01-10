a02e0dfac5cf12fb828be242e7d559e61587517c
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2025-01-09 20:05:09 -0800

    make ball bounce

--- ping-pong/ball.py
+++ ping-pong/ball.py
@@ -6,8 +6,16 @@ class Ball(Turtle):
         self.color("white")
         self.shape("square")
         self.penup()
+        self.x_axis = 10
+        self.y_axis = 10
+
+    def bounce_y(self):
+        self.y_axis *= -1
+    def bounce_x(self):
+        self.x_axis *= -1
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_axis
+        new_y = self.ycor() + self.y_axis
         self.goto(new_x, new_y)
+
--- ping-pong/pong.py
+++ ping-pong/pong.py
@@ -33,5 +33,11 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    if ball.distance(r_player) < 50 and ball.xcor() > 320 or ball.distance(l_player) < 50 and ball.xcor() < -320:
+        print("Made contact")
+        ball.bounce_x()
+
 
 screen.exitonclick()
