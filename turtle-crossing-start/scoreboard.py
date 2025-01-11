d95434e7b0322b1c172416d6af81af260558b0d8
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2025-01-10 16:49:38 -0800

    create score for turtle crossing game

--- turtle-crossing-start/scoreboard.py
+++ turtle-crossing-start/scoreboard.py
@@ -1,5 +1,23 @@
+from idlelib.configdialog import font_sample_text
+from os import write
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.level = 0
+        self.goto(0, 260)
+        self.update_scoreboard()
+
+
+    def update_scoreboard(self):
+        self.clear()
+        self.level += 1
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
