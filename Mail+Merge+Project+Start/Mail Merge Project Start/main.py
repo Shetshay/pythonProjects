21bd360244e915b9a309efb6de5fc8f74b4a68e7
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2025-01-14 23:38:27 -0800

    create mailbox like message sender

--- Mail+Merge+Project+Start/Mail Merge Project Start/main.py
+++ Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -1,5 +1,20 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
+#TODO: Create a letter using starting_letter.txt
+
+with open("./Input/Names/invited_names.txt", mode="r") as names:
+    invitedNames = names.readlines() #store invited names into a list using readlines
+
+for name in invitedNames:
+    filepath = f"./Output/ReadyToSend/{name}.txt" # output has an error where name\n
+    newFilepath = filepath.replace("\n", "") # fix \n error by replacing it
+    #print(newFilepath)
+    with open(newFilepath, mode = 'w') as names: # write to file, if it doesn't exist create it
+        new_name = name.strip() # name will make comma go to next line for some reason, use strip to remove special char
+        content = names.write(f"Dear {new_name},\n\n" # weird how we have to give it a variable to write in a file
+            "You are invited to my birthday this Saturday.\n\n"
+            "Hope you can make it!\n\n"
+            "Andrew")
+
+
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
     
