d78fbdb6a1a2213a988ed8af1a61971e3deb7f51
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2025-01-16 15:54:50 -0800

    update temperature using pandas and add squirrel data csv

--- pandas/main.py
+++ pandas/main.py
@@ -1,5 +1,33 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(type(data))
-#print(data["temp"])
+# #print(type(data))
+# #print(data["temp"])
+#
+# data_dict = data.to_dict()
+# #print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# #print(temp_list)
+#
+# print(data["temp"].max())
+
+#print(data[data.day == "Monday"])
+
+#print(data[data.temp == (data["temp"].max())])
+
+monday = data[data.day == "Monday"]
+
+# convert to Fahrenheit from Celsius
+print(monday.replace(monday.temp[0], (monday.temp[0] * (9/5) + 32)))
+
+
+
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data_school = pandas.DataFrame(data_dict)
+print(data_school)
+data_school.to_csv("new_data.csv") # create new data
