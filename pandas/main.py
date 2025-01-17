c9520363a71097b43269bbbb0af6c77f0dd5d3df
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2025-01-16 17:44:58 -0800

    update temperature using pandas and add squirrel data csv

--- pandas/main.py
+++ pandas/main.py
@@ -1,33 +1,54 @@
 import pandas
+#def practice():
+    #data = pandas.read_csv("weather_data.csv")
+    # #print(type(data))
+    # #print(data["temp"])
+    #
+    # data_dict = data.to_dict()
+    # #print(data_dict)
+    #
+    # temp_list = data["temp"].to_list()
+    # #print(temp_list)
+    #
+    # print(data["temp"].max())
 
-data = pandas.read_csv("weather_data.csv")
-# #print(type(data))
-# #print(data["temp"])
-#
-# data_dict = data.to_dict()
-# #print(data_dict)
-#
-# temp_list = data["temp"].to_list()
-# #print(temp_list)
-#
-# print(data["temp"].max())
+    #print(data[data.day == "Monday"])
 
-#print(data[data.day == "Monday"])
+    #print(data[data.temp == (data["temp"].max())])
 
-#print(data[data.temp == (data["temp"].max())])
+    # monday = data[data.day == "Monday"]
 
-monday = data[data.day == "Monday"]
-
-# convert to Fahrenheit from Celsius
-print(monday.replace(monday.temp[0], (monday.temp[0] * (9/5) + 32)))
+    # convert to Fahrenheit from Celsius
+    # print(monday.replace(monday.temp[0], (monday.temp[0] * (9/5) + 32)))
 
 
 
-# Create a dataframe from scratch
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
+    # Create a dataframe from scratch
+    # data_dict = {
+    #     "students": ["Amy", "James", "Angela"],
+    #     "scores": [76, 56, 65]
+    # }
+    # data_school = pandas.DataFrame(data_dict)
+    # print(data_school)
+    # data_school.to_csv("new_data.csv") # create new data
+
+#practice()
+
+squirrel_data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20250116.csv")
+#print(squirrel_data["Primary Fur Color"])
+
+#print(squirrel_data["Primary Fur Color"].unique())
+(squirrel_data["Primary Fur Color"].unique())
+
+squirrel_fur_color = (squirrel_data["Primary Fur Color"].unique()[1::])
+
+
+fur_color_count = {
+    "Fur Color": squirrel_fur_color,
+    #"Count": (squirrel_data["Primary Fur Color"])
 }
-data_school = pandas.DataFrame(data_dict)
-print(data_school)
-data_school.to_csv("new_data.csv") # create new data
+
+squirrel_query = pandas.DataFrame(fur_color_count)
+squirrel_query.to_csv("squirrel_query.csv") # create new data
+
+print(squirrel_query)
