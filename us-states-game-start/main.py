3c4b93ebcb567ac0fad903a66ebe80054a758493
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2025-01-19 12:39:17 -0800

    complete 50 state game

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -1,12 +1,43 @@
 import turtle
+from os import write
+from turtle import Turtle, Screen
+import pandas
+from score import Score
+
+ALIGNMENT = 'center'
+FONT = ('Courier', 24, 'normal')
 
 screen = turtle.Screen()
+screen.setup(width=725, height=491)
 screen.title("U.S. States Game")
 image = "./blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+score = Score()
 
+data = pandas.read_csv("50_states.csv")
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
-print(answer_state)
+amount_of_states = len(data.state)
+end_game = False
+
+state = Turtle()
+state.penup()
+state.speed("fastest")
+state.shape("circle")
+state.shapesize(0.2)
+
+while not end_game:
+    answer_state = screen.textinput(title=f"{score.score}/{amount_of_states}", prompt="What's another state's name?")
+    for guess in range(amount_of_states):
+        if data.state[guess].upper() == answer_state.upper():
+            score.update_score()
+            state.goto(data.x[guess], data.y[guess])
+            state.write(answer_state)
+    if score.score >= amount_of_states:
+        state.hideturtle()
+        state.goto(0,0)
+        state.write("Congratulations, you won!", align = ALIGNMENT, font = FONT)
+        end_game = True
+
+screen.exitonclick()
