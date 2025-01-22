f08736dca68212878a584a3cfb58516144413c67
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2025-01-22 15:49:10 -0800

    edit comments

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,26 +1,4 @@
-    # student_dict = {
-    #     "student": ["Angela", "James", "Lily"],
-    #     "score": [56, 76, 98]
-    # }
-    #
-    # #Looping through dictionaries:
-    # for (key, value) in student_dict.items():
-    #     #Access key and value
-    #     pass
-    #
-    # import pandas
-    # student_data_frame = pandas.DataFrame(student_dict)
-    #
-    # #Loop through rows of a data frame
-    # for (index, row) in student_data_frame.iterrows():
-    #     #Access index and row
-    #     #Access row.student or row.score
-    #     pass
-
-    # Keyword Method with iterrows()
-    # {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
+# 1. Create a dictionary in this format:
 import pandas
 {"A": "Alfa", "B": "Bravo"}
 phonetics = pandas.read_csv("nato_phonetic_alphabet.csv")
@@ -31,7 +9,7 @@ for (index, row) in phonetics.iterrows():
     #Access row.letter or row.code
 print(phonetics_dict) # print the final result of that
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# 2. Create a list of the phonetic code words from a word that the user inputs.
 user_input = input("Enter a word: ").upper() # make sure the input is uppercase
 output = [] # instantiate output array for later use
 arrayed_user_input = list(user_input) # make the user input a list/array
