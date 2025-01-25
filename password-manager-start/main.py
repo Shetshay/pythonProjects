da26a7188656ebc99aeebf79b02d4532c96045aa
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2025-01-25 13:12:19 -0800

    upload progress for password manager project

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -17,32 +17,32 @@ canvas.create_image(100, 100, image=lock_img)
 canvas.grid(column=1 , row=1 )
 
 
-#Labels
+#Website Label
 website_title = Label(text="Website: ")
 website_title.grid(column=0 , row=2 )
-#Text
+#Website Text
 website = Entry(width=35)
-website.grid(column=1 , row=2 )
+website.grid(column=1 , row=2, columnspan=2)
 
-#Labels
+#Email/Username Label
 email_username_title = Label(text="Email/Username: ")
 email_username_title.grid(column=0 , row=3 )
-#Text
+#Email/Username Text
 email_username = Entry(width=35)
-email_username.grid(column=1 , row=3 )
+email_username.grid(column=1 , row=3, columnspan=2)
 
-#Labels
+#Password Label
 password_title = Label(text="Password: " )
 password_title.grid(column=0 , row=4)
-#Text
-password = Entry(width=21)
-password.grid(column=1 , row=4 , columnspan=2)
+#Password Text
+password_text = Entry(width=21)
+password_text.grid(column=1 , row=4)
 
-#Buttons
-generate_password = Button(text="Generate Password", width=14)
+#Generate Password Button
+generate_password = Button(text="Generate Password")
 generate_password.grid(column=2 , row=4)
 
-#Buttons
+#Add Button
 add = Button(text="Add", width=36)
 add.grid(column=1 , row=5, columnspan=2)
 
