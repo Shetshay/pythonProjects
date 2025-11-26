6d3217f6b763bc3333fa9e75a17b01ab36a4f8de
Andrew Attia Ibrahim <and.ibrahim.rew@gmail.com>
2025-11-25 21:26:33 -0800

    add issoverhead project

--- day33/main.py
+++ day33/main.py
@@ -1,7 +1,26 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 51.507351
+MY_LONG = -0.127758
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+# http://api.open-notify.org/iss-now.json # saved
+
+response = requests.get(url="http://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-print(data)
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+
+print(time_now.hour)
---
+++ issoverhead-start/main.py
@@ -0,0 +1,54 @@
+import requests
+from datetime import datetime
+
+MY_LAT = 35.373291 # Your latitude 35.373291
+MY_LONG = -119.018715 # Your longitude -119.018715
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+#Your position is within +5 or -5 degrees of the ISS position.
+
+def iss_location():
+    if MY_LONG - 5 <= iss_longitude <= MY_LONG + 5 and MY_LAT - 5 <= iss_latitude <= MY_LAT + 5:
+        return True
+    else:
+        return False
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now()
+
+#If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+result = iss_location()
+
+print(sunset)
+print(sunrise)
+print(time_now.hour)
+
+#if result and sunrise
+
+
+
+# BONUS: run the code every 60 seconds.
+
+# print(iss_latitude)
+# print(iss_longitude)
+
+
